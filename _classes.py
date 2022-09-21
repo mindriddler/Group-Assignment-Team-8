da9dac695cce940db1cc4fe12c3542679b829871
Wikipedia11 <raekon92@gmail.com>
2022-09-21 22:23:20 +0200

    Made changed and improvements here

--- _classes.py
+++ _classes.py
@@ -1,52 +1,52 @@
-import sqlite3
-from db_insert import db_path
-from db_insert import cursor
-from db_insert import sqliteConnection
+import db_insert
 
-
-sqliteConnection = sqlite3.connect(db_path)
-cursor = sqliteConnection.cursor()
+# def print_one(connection):   
+#     cursor = connection.cursor()
+#     result = cursor.execute('select * from persons;').fetchall()
+#     return result
     
-# Show the table
-cursor.execute('select * from persons;')
 
-# cursor.execute("INSERT INTO persons VALUES (?, ?, ?, ?)", (person_1.firstname, person_1.lastname, person_1.birthdate, person_1.address))
+def getlimitedRows(connection, size):
+    
+        cursor = connection.cursor()
 
-# sqliteConnection.commit()
-# View result. Remove comment to print data from the DB
-result = cursor.fetchone()
-
-
-
+        sqlite_select_query = 'SELECT * from persons'
+        cursor.execute(sqlite_select_query)
+        records = cursor.fetchmany(size)
+        print("Fetching a total of", size,"rows")
+        print("Printing each row")
+        for row in records:
+            print("\n")
+            print("Firstname:", row[1])
+            print("Lastname:", row[2])
+            print("Birthdate:", row[3])
+            print("Address:", row[4])
+            print("\n")
 
 class Person:
     
-    def __init__(self,index, firstname, lastname, birthdate, address):
-        self.index = index
+    def __init__(self, firstname, lastname, birthdate, address):
+        # self.index = index
         self.firstname = firstname
         self.lastname = lastname
         self.birthdate = birthdate
         self.address = address
      
-    
-        
     def __str__(self):
-        return f"{self.index}, {self.firstname} {self.lastname} is born {self.birthdate} and lives on {self.address}"
+        return f"{self.firstname} {self.lastname} is born {self.birthdate} and lives on {self.address}"
+
+# def make_func_to_str_and_print(connection):
+#     result = print_one(connection)
+#     person_1 = Person(result[1], result[2], result[3], result[4])
+#     print(person_1)
 
 
-person_1 = Person(result[0], result[1], result[2], result[3], result[4])
-print(person_1)
 
-
-
-
-"""class Person:
-    def init(self,index, first, last, birth, address):
-        self.index = index
-        self.first = first
-        self.last = last
-        self.birth = birth
-        self.address = address
-
-    def str(self):
-        return f"{self.index}: {self.first} {self.last}, born {self.birth}, living on {self.address}"""
+def clazz():
+    with db_insert.conn_to_db('db\SQLiteDB.db') as conn:
+        # print("Connection to SQL open.")
+        db_insert.cursor(conn)
+        getlimitedRows(conn, 6)
+        # print_one(conn)
+        # make_func_to_str_and_print(conn)
+    db_insert.close_connection(conn)
