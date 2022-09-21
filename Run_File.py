7b4f5e4738de9ae0e78d8fa26f2fb9ef83c5a41a
Wikipedia11 <raekon92@gmail.com>
2022-09-21 20:51:11 +0200

    clean up and improvements

---
+++ Run_File.py
@@ -0,0 +1,10 @@
+import db_insert
+import _classes
+
+# if __name__ == '__main__':
+#     db_insert.main()
+    
+
+
+if __name__ == '__main__':
+    _classes.clazz()
--- db_insert.py
+++ db_insert.py
@@ -2,62 +2,72 @@ import sqlite3
 import csv
 
 
-
 # Path to csv file. Important to write it as C:\Users\Fredrik\Desktop\Repos\Nackademin\Programmering_Systemering\GroupAssignmentDevOps22\data\persons.csv" 
 # Otherwise it might not work. You will need to specify your path to your copy of the file ofcourse
+def file_input():
+    file_path = input("Please enter the file path: ") 
+    return file_path
+# def db_input():   
+#     db_path = input("Please enter the path to the DB: ")
+#     return db_path
 
-file_path = input("Please enter the file path: ") 
-db_path = input("Please enter the path to the DB: ")
-# C:\Users\Fredrik\Desktop\Repos\Nackademin\Programmering_Systemering\GroupAssignmentDevOps22\data\persons.csv
+def conn_to_db(filename):
+    return sqlite3.connect(filename)
 
-try:
-  
+def cursor(connection):   
+    cursor = connection.cursor()
+    return cursor
+
+
+def import_csv_file():  
     # Import csv and extract data, this is where we need to start. 
     # Basicly open up the CSV file and importing the raw text into the DB
-    with open(file_path, 'r') as fin:
+    with open(file_input(), 'r') as fin:
         dict_reader = csv.DictReader(fin)
         persons = [(i['Index'], i['Firstname'], i['Lastname'], i["Birthdate"], i["Address"]) for i in dict_reader]
-        # print(persons)
-  
-    # Connect to SQLite. 
-    # This creates the connection to the database, which is needed to make changes to it. Its not the same as writing a textfile or w/e
-    sqliteConnection = sqlite3.connect(db_path)
-    cursor = sqliteConnection.cursor()
-  
+        return persons
+            
+def create_table(connection):
     # Remove comment to create table, but after the table is created, comment this line back
     # This is where we create the table.
-    cursor.execute('create table persons (`index`, firstname, lastname, birthdate, address);')
-  
+    connection.execute('CREATE TABLE IF NOT EXISTS persons (`index`, firstname, lastname, birthdate, address, CONSTRAINT name_unique UNIQUE (firstname, lastname, address));')
+    connection.commit()
+            
+def insert_to_db(connection): 
     # Remove comment to insert data, but after the data is inserted, comment this line back 
     # And this is where the data from the persons.csv file get imported
-    cursor.executemany("insert into persons (`index`, firstname, lastname, birthdate, address) VALUES (?, ?, ?, ?, ?);", persons)
-  
+    connection.executemany('INSERT OR IGNORE INTO persons (`index`, firstname, lastname, birthdate, address) VALUES (?, ?, ?, ?, ?);', import_csv_file())
+    connection.commit()
+            
+def execute_select(cursor):
     # Show the table
-    cursor.execute('select * from persons;')
-  
-    # View result. Remove comment to print data from the DB
-    result = cursor.fetchall()
-    print(result)
-  
-    # Commit work and close connection. 
-    # Think Git :') 
-    # We need to commit our changes to the database for it in order for it to get updated with the new information
-    # If we want to make sure that our changes get commited to the database,
-    # we can just add sqliteConnection.commit() after each execute command that changes stuff
-    
-    sqliteConnection.commit() 
-    cursor.close()
-    
-# except sqlite3.Error as error:
-        # print('Error occured - ', error)
-except FileNotFoundError as error2:
-    print("Wrong path specified -", error2)
+    result = cursor.execute('SELECT * FROM persons')
+    return result
+
+def print_fetch_all(result): 
+     # View result. Remove comment to print data from the DB
+    for all in result:
+        print(all)
+
  
 # I didnt like it when the console was filled with error text so i made a few more try/except to intercept all of the possible errors
-# try:
-    # if sqliteConnection:
-        # sqliteConnection.close()
-        # print('SQLite Connection closed')
-# except NameError as error3:
-    # print("Wrong path specified -", error3)
+def close_connection(connection):
+    if connection:
+        connection.close()
+        print('SQLite Connection closed')
+
+     
     
+    
+def main():
+    with conn_to_db('db\SQLiteDB.db') as conn:
+        print("Connection to SQL open.")
+        cursor(conn)
+        create_table(conn)
+        insert_to_db(conn)
+        # execute_select(cursor(conn))
+        print_fetch_all(execute_select(conn))
+    close_connection(conn)
+    
+    
+
