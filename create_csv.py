137ac9831b27227901afc46ebf761bc5e7520c13
Wikipedia11 <raekon92@gmail.com>
2022-09-21 22:24:22 +0200

    changed how we create csv files

--- create_csv.py
+++ create_csv.py
@@ -2,46 +2,44 @@ import csv
 
 persons = [
     {
-        "Index": 1,
-        "Firstname": "pelle",
-        "Lastname": "svensson",
+        "id": 1,
+        "Firstname": "Pelle",
+        "Lastname": "Svensson",
         "Birthdate": "94-02-15",
-        "Address": "lillgatangatan 4"
+        "Address": "Lillgatan 4"
     },
         {
-        "Index": 2,
-        "Firstname": "nina",
-        "Lastname": "bengtsson",
+        "id": 2,
+        "Firstname": "Nina",
+        "Lastname": "Bengtsson",
         "Birthdate": "99-02-02",
-        "Address": "storgatan 4"
+        "Address": "Storgatan 4"
     },
         {
-        "Index": 3,
-        "Firstname": "nisse",
-        "Lastname": "svensson",
+        "id": 3,
+        "Firstname": "Nisse",
+        "Lastname": "Svensson",
         "Birthdate": "67-04-12",
-        "Address": "bergsvägen 11"
+        "Address": "Bergsvägen 11"
     },
         {
-        "Index": 4,
-        "Firstname": "johan",
-        "Lastname": "persson",
+        "id": 4,
+        "Firstname": "Johan",
+        "Lastname": "Persson",
         "Birthdate": "94-07-15",
-        "Address": "inteckningsvägen 1"
+        "Address": "Inteckningsvägen 1"
     },
         {
-        "Index": 5,
-        "Firstname": "karl",
-        "Lastname": "nilsson",
+        "id": 5,
+        "Firstname": "Karl",
+        "Lastname": "Nilsson",
         "Birthdate": "84-02-27",
-        "Address": "sedelvägen 5"
+        "Address": "Sedelvägen 5"
     },    
 ] 
-
-
-
-# https://docs.python.org/3/library/csv.html
-with open(r"C:\Users\Fredrik\Desktop\Repos\Nackademin\Programmering_Systemering\GroupAssignmentDevOps22\data\persons.csv", 'w+') as f:
-    csv_writer = csv.DictWriter(f, fieldnames=persons[0].keys())
-    csv_writer.writeheader()
-    csv_writer.writerows(persons)
+def create_csv():
+    with open('data\persons.csv', 'w', encoding='UTF-8', newline='') as f:
+        # writer = csv.writer(csvarchive)
+        csv_writer = csv.DictWriter(f, fieldnames=persons[0].keys())
+        csv_writer.writeheader()
+        csv_writer.writerows(persons)
