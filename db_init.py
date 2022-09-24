79b9ed941ec0650bcf2c5eb2ff640eaee68c1571
Martin Alfredson <martin.alfredson@hotmail.com>
2022-09-24 19:02:04 +0200

    Added option to go back in load data from file.

--- db_init.py
+++ db_init.py
@@ -3,12 +3,12 @@ import csv
 
 
 def file_input():
-    while True:
-        try:
-            file_path = input("Please enter the file path: ") 
-            return file_path
-        except ValueError:
-            print("Not a valid input, try again")
+    from menu_test import Menu
+    file_path = input("Please enter the file path: ")
+    if file_path == 'b':
+        print("Returning to main menu")
+        Menu().menu_loop()
+    return file_path
 
 def conn_to_db(filename):
     return sqlite3.connect(filename)
@@ -27,9 +27,9 @@ def import_csv_file():
                 persons = [(i['Firstname'], i['Lastname'], i["Birthdate"], i["Address"], i['Fullname']) for i in dict_reader]
                 return persons
         except FileNotFoundError:
-            print("Not a valid path, try again")
+            print("Not a valid path, try again or 'b' to go back.")
         except PermissionError:
-            print("Not a valid path, try again")
+            print("Not a valid path, try again or 'b' to go back.")
             
 def create_table(connection):
     connection.execute('CREATE TABLE IF NOT EXISTS persons (firstname, lastname, birthdate, address, fullname,  CONSTRAINT name_unique UNIQUE (firstname, lastname, address));')
